20d4c6edd896cbc1b23601906348b3b931857493
Yu-Hao <itoyuhao@gmail.com>
2022-09-05 23:56:46 +0800

    Added step.py and get_video_list.py part1

---
+++ yt_concat/steps/get_video_list.py
@@ -0,0 +1,37 @@
+# 理想的import 順序:
+# 1. 內建
+# 2. 第三方
+# 3. 自己寫的
+import urllib.request
+import json
+
+from yt_concat.steps.step import Step, StepException
+from settings import API_KEY
+
+
+class GetVideoList(Step):
+    def process(self, inputs):
+        channel_id = inputs['channel_id']
+        base_video_url = 'https://www.youtube.com/watch?v='
+        base_search_url = 'https://www.googleapis.com/youtube/v3/search?'  # API endpoint 讓我們使用API的網址
+
+        first_url = base_search_url+'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(API_KEY, channel_id)
+
+        video_links = []
+        url = first_url
+
+        while True:
+            inp = urllib.request.urlopen(url)
+            resp = json.load(inp)
+
+            for i in resp['items']:
+                if i['id']['kind'] == "youtube#video":
+                    video_links.append(base_video_url + i['id']['videoId'])
+
+            try:
+                next_page_token = resp['nextPageToken']
+                url = first_url + '&pageToken={}'.format(next_page_token)
+            except KeyError:
+                break
+        return video_links
+
---
+++ yt_concat/steps/step.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractclassmethod
+
+class Step(ABC):
+    
+    # 如果把init function也定義成 abstract method 則所有子孫class都要implement他(要把它寫出來)，在此不需要
+    def __init__(self):
+        pass
+
+
+    @abstractclassmethod
+    def process(self, inputs):
+        pass
+
+class StepException(Exception):
+    '''
+    這邊設定此class的用意是，任何一個步驟有觸發設計的這種StepException，這整個流程就停止
+    function裡的Exception是python built-in的，不需要另外import
+    '''
+    pass
